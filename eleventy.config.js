1e80650c4c880d986a11050887d04d4e8c608276
Joeweh <joeyqsa@outlook.com>
2026-09-03 02:32:28 -0400

    Add resume to eleventy output

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/CNAME");
     eleventyConfig.addPassthroughCopy("src/robots.txt");
     eleventyConfig.addPassthroughCopy("src/sitemap.xml");
-    eleventyConfig.addPassthroughCopy("src/**/*.{css,js,png,jpg,webp,svg,ico,woff2}");
+    eleventyConfig.addPassthroughCopy("src/**/*.{css,js,png,jpg,webp,svg,ico,woff2,pdf}");
 
     return {
         dir: { input: "src", output: "_site" },

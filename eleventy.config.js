12173ddf0f08b814bacf19aa1b4aa0a4dc034b3c
Joeweh <joeyqsa@outlook.com>
2026-09-04 04:47:30 -0400

    Tighten eleventy passthrough copy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,9 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/CNAME");
     eleventyConfig.addPassthroughCopy("src/robots.txt");
-    eleventyConfig.addPassthroughCopy("src/**/*.{css,js,png,jpg,webp,svg,ico,woff2,pdf}");
+    eleventyConfig.addPassthroughCopy("src/favicon.ico");
+    eleventyConfig.addPassthroughCopy("src/styles.css");
+    eleventyConfig.addPassthroughCopy("src/assets");
 
     return {
         dir: { input: "src", output: "_site" },
